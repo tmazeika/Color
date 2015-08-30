4e951a63fe03aab4b4fb767a915941580580fc68
T.J. Mazeika <t@mazeika.me>
2015-08-30 04:14:47 -0400

    Revamp app

--- js/script.js
+++ js/script.js
@@ -1,173 +1,124 @@
-var ColorFormats = {
-    HEX: "hex",
-    RGBA: "rgba",
-    HSLA: "hsla"
-};
-
-var currentColorFormat = ColorFormats.RGBA;
-var previousInputs;
-var previousColor;
-var currentColor;
-
-function trackInput(e)
-{
-    var lastVal;
-
-    setInterval(function() {
-        var thisVal = $(e).val();
-
-        if ( ! lastVal || lastVal !== thisVal) updateColor();
-
-        lastVal = thisVal;
-    }, 10)
-}
-
 function rand(min, max)
 {
-    return Math.floor( Math.random() * (max - min + 1) ) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function toPercent(x)
 {
-    return Math.round( x * 100 ) + "%";
+    return Math.round(x * 100) + '%';
 }
 
-// read the inputted color info and update
-function updateColor()
-{
-    // read the correct color input type (rgba, hsla, or hex)
-    switch (currentColorFormat)
-    {
-        case ColorFormats.HEX:
-            var hex = $("#hex-input").val();
-            var hexA = 1;
+function updateColor(inputType) {
+    var hex = $('#hex-input').val();
 
-            if (hex.charAt(0) !== "#")
-            {
-                hex = "#" + hex;
-            }
+    if (hex[0] !== '#') {
+        hex = '#' + hex;
+    }
 
-            // if we have information about the alpha, use it
-            if (hex.length === 8 || hex.length === 9)
-            {
-                // get the last 1 or 2 characters to get the alpha info
-                var lastSubStr = hex.substr( hex.length - (hex.length === 8 ? 1 : 2) );
+    var rgbR = $('#rgb-r-input').val();
+    var rgbG = $('#rgb-g-input').val();
+    var rgbB = $('#rgb-b-input').val();
 
-                // convert the hex alpha info to decimal then get the percentage (for jQuery Color)
-                hexA = parseInt(lastSubStr, 16) / 255;
-            }
+    var hslH = $('#hsl-h-input').val();
+    var hslS = $('#hsl-s-input').val();
+    var hslL = $('#hsl-l-input').val();
 
-            currentColor = $.Color(hex);
-            currentColor = currentColor.alpha(hexA);
+    var color;
 
+    switch (inputType) {
+        case 'hex':
+            color = $.Color(hex);
             break;
-
-        case ColorFormats.RGBA:
-            var rgbaR = $("#rgba-r-input").val();
-            var rgbaG = $("#rgba-g-input").val();
-            var rgbaB = $("#rgba-b-input").val();
-            var rgbaA = $("#rgba-a-input").val();
-
-            currentColor = $.Color(rgbaR, rgbaG, rgbaB, rgbaA);
+        case 'rgb':
+            color = $.Color(rgbR, rgbG, rgbB);
             break;
-
-        case ColorFormats.HSLA:
-            var hslaH = $("#hsla-h-input").val();
-            var hslaS = $("#hsla-s-input").val();
-            var hslaL = $("#hsla-l-input").val();
-            var hslaA = $("#hsla-a-input").val();
-
-            currentColor = $.Color({ hue: hslaH, saturation: hslaS, lightness: hslaL });
-            currentColor = currentColor.alpha(hslaA);
+        case 'hsl':
+            color = $.Color({hue: hslH, saturation: hslS, lightness: hslL});
             break;
     }
 
-    previousColor = currentColor;
+    var hex = color.toHexString(false).toUpperCase();
 
-    var alpha = currentColor.alpha();
-    var alphaPercent = toPercent(alpha);
+    var red = color.red();
+    var green = color.green();
+    var blue = color.blue();
 
-    var hex = currentColor.toHexString(alpha !== 1).toUpperCase();
-
-    var red = currentColor.red();
-    var green = currentColor.green();
-    var blue = currentColor.blue();
-
-    var hue = currentColor.hue();
-    var saturation = currentColor.saturation();
+    var hue = color.hue();
+    var saturation = color.saturation();
     var saturationPercent = toPercent(saturation);
-    var lightness = currentColor.lightness();
+    var lightness = color.lightness();
     var lightnessPercent = toPercent(lightness);
 
     // update all inputs
-    if (currentColorFormat !== ColorFormats.HEX)
-    {
-        $("#hex-input").val(hex);
+    if (inputType !== 'hex') {
+        $('#hex-input').val(hex);
     }
 
-    if (currentColorFormat !== ColorFormats.RGBA)
-    {
-        $("#rgba-r-input").val(red);
-        $("#rgba-g-input").val(green);
-        $("#rgba-b-input").val(blue);
-        $("#rgba-a-input").val(alpha);
+    if (inputType !== 'rgb') {
+        $('#rgb-r-input').val(red);
+        $('#rgb-g-input').val(green);
+        $('#rgb-b-input').val(blue);
     }
 
-    if (currentColorFormat !== ColorFormats.HSLA)
-    {
-        $("#hsla-h-input").val(hue);
-        $("#hsla-s-input").val(saturation);
-        $("#hsla-l-input").val(lightness);
-        $("#hsla-a-input").val(alpha);
+    if (inputType !== 'hsl') {
+        $('#hsl-h-input').val(hue);
+        $('#hsl-s-input').val(saturation);
+        $('#hsl-l-input').val(lightness);
     }
 
-    // update all results
-    $(".color-preview").css("background-color", currentColor.toRgbaString());
 
-    $("#hex-output").text(hex);
+    // update all outputs
+    $('#rgb-r-output').text(red);
+    $('#rgb-g-output').text(green);
+    $('#rgb-b-output').text(blue);
 
-    $("#rgba-r-output").text(red);
-    $("#rgba-g-output").text(green);
-    $("#rgba-b-output").text(blue);
-    $("#rgba-a-output").text(alphaPercent);
+    $('#hsl-h-output').text(hue);
+    $('#hsl-s-output').text(saturationPercent);
+    $('#hsl-l-output').text(lightnessPercent);
 
-    $("#hsla-h-output").text(hue);
-    $("#hsla-s-output").text(saturationPercent);
-    $("#hsla-l-output").text(lightnessPercent);
-    $("#hsla-a-output").text(alphaPercent);
+    $('#hex-output').text(hex).data('manually-changed', true);
+
+    // update color preview
+    $('.color-preview').css('background-color', color.toRgbaString());
 }
 
 $(function() {
     // start out with a random color by default for fun
-    $("#rgba-r-input").val( rand(0, 255) );
-    $("#rgba-g-input").val( rand(0, 255) );
-    $("#rgba-b-input").val( rand(0, 255) );
+    $('#rgb-r-input').val(rand(0, 255));
+    $('#rgb-g-input').val(rand(0, 255));
+    $('#rgb-b-input').val(rand(0, 255));
 
-    // initialize color
-    updateColor();
+    updateColor('rgb');
 
-    // lock the color control container dimensions
-    var colorControlContainer = $(".color-control-container");
-    colorControlContainer.css('width', colorControlContainer.css('width') );
-    colorControlContainer.css('height', colorControlContainer.css('height') );
+    $('input[type=range]')
+        .on('mousedown', function() {
+            $(this).data('being-changed', true);
+        })
+        .on('mouseup', function() {
+            $(this).data('being-changed', false);
+        })
+        .each(function(i, input) {
+            setInterval(function() {
+                var beingChanged = $(input).data('being-changed');
 
-    // track input changes
-    $.each($("input"), function(i, val)
-    {
-        trackInput(val);
-    });
+                if (! beingChanged) return;
 
-    // show the correct controls when a color button is clicked
-    $(".color-format-button").click(function()
-    {
-        currentColorFormat = $(this).data("type");
+                var lastValue = $(input).data('last-value');
+                var newValue = $(input).val();
 
-        // remove all active buttons then add the class to the clicked button
-        $(".color-format-button").removeClass("active");
-        $(this).addClass("active");
+                if (lastValue === newValue) return;
 
-        // hide all inputs then show the one we need
-        $(".color-inputs").hide();
-        $(".color-inputs[data-type='" + currentColorFormat + "']").show();
+                $(input).data('last-value', newValue);
+
+                var inputType = $(input).closest('.input-container').attr('data-type');
+
+                updateColor(inputType);
+            }, 8 /* roughly 120 FPS */);
+        });
+
+    $('#hex-input').on('keyup', function() {
+        var inputType = $(this).closest('.input-container').attr('data-type');
+
+        updateColor(inputType);
     });
 });
